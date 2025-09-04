e4340e96029634ab55150c4c084e0f569c1fc957
alevan10 <levan.andrew.w@gmail.com>
2025-09-04 14:16:07 -0400

    ensure docker run works without issue

--- run_in_cli.py
+++ run_in_cli.py
@@ -8,16 +8,16 @@ from langchain.vectorstores import Chroma
 from langchain_ollama  import OllamaEmbeddings
 
 
-DOCUMENT_DIR = Path(__file__).parent.parent.joinpath("documents")
+DOCUMENT_DIR = Path(__file__).parent.joinpath("documents")
 
 
 if __name__ == "__main__":
     loader = DirectoryLoader(str(DOCUMENT_DIR), glob="./*.pdf", loader_cls=PyPDFLoader)
     processed_pdf_documents = loader.load()
 
-    vector_store = Chroma.from_documents(documents=processed_pdf_documents, embedding=OllamaEmbeddings(base_url="http://localhost:7869", model="nomic-embed-text:v1.5"))
+    vector_store = Chroma.from_documents(documents=processed_pdf_documents, embedding=OllamaEmbeddings(base_url="http://host.docker.internal:7869", model="nomic-embed-text:v1.5"))
 
-    llm = Ollama(base_url="http://localhost:7869", model="gpt-oss:20b", verbose=True)
+    llm = Ollama(base_url="http://host.docker.internal:7869", model="gpt-oss:20b", verbose=True)
     qa_chain = RetrievalQA.from_chain_type(llm, retriever=vector_store.as_retriever())
     result = qa_chain({"query": "Your job is to look for numbers within the provided context. We want to find the largest number anywhere in the provided context. The unit is not important (could be dollars, years, pounds, etc), we're just looking for the greatest numerical value in the document. When the number is found return it, you are required to return it as well as the page number of the document and the surrounding text to help verify the result."})
     print(result["result"])
